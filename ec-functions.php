296b2315855d29a1e403c97262413de38bdb4a87
m-e-h <info@martyhelmick.com>
2019-06-28 12:57:54 -0400

    v1.1.0

--- ec-functions.php
+++ ec-functions.php
@@ -3,7 +3,7 @@
  * Plugin Name: EC Functions
  * Plugin URI:  https://github.com/DioceseOfCharlotte/ec-functions
  * Description: CSS debugger
- * Version:     1.0.0
+ * Version:     1.1.0
  * Author:      Billy Griffith
  * Author URI:  https://github.com/DioceseOfCharlotte
  * Text Domain: doc-ec
