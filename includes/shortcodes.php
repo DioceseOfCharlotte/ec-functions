66c19294c47a8f7e3585e897dcce8e52605b0b3b
m-e-h <info@martyhelmick.com>
2019-07-29 15:19:27 -0400

    add column option to gf list shortcode

--- includes/shortcodes.php
+++ includes/shortcodes.php
@@ -8,29 +8,38 @@ function ec_register_shortcodes() {
 }
 
 // Gravity Forms listrow.
+// [ec-listrow entry="{entry_id}" field="27" row="3" column_title="Title of Column"]
 function ec_listrow_shortcode( $atts ) {
 
 	// Attributes
 	$atts = shortcode_atts(
 		array(
-			'entry' => '1',
-			'field' => '1',
-			'row'   => '0',
+			'entry'        => '1',
+			'field'        => '1',
+			'row'          => '0',
+			'column_title' => '',
 		),
 		$atts,
 		'ec-listrow'
 	);
 
-	$entry = GFAPI::get_entry( $atts['entry'] );
-	$row   = $atts['row'];
-	$field = $atts['field'];
+	$entry  = GFAPI::get_entry( $atts['entry'] );
+	$row    = $atts['row'];
+	$field  = $atts['field'];
+	$column = $atts['column'];
 
 	$lists = maybe_unserialize( rgars( $entry, $field ) );
 
 	$listrow = '';
 
 	foreach ( $lists[ $row ] as $rows => $cell ) {
-		$listrow .= $cell . ' ';
+		$listrow .= $cell;
+		$listrow .= ' ';
+	}
+	$listrow = rtrim( $listrow );
+
+	if ( $column ) {
+		$listrow = $lists[ $row ][ $column ];
 	}
 
 	return $listrow;
